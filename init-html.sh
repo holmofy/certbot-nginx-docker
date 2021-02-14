d34c14507b1e020e12d189f04ab3ce9bfe8ebf07
Holmofy Hu <hff1996723@163.com>
2021-02-14 16:52:42 +0800

    init-html

--- init-html.sh
+++ init-html.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# access url:
+# $user@$host:~/.repo/$1.git
 init_bare_git_repo() {
-    bare_git_dir="~/.${1}.git"
+    bare_git_dir="${HOME}/.repo/${1}.git"
     git init --bare $bare_git_dir
-    echo "#!/bin/sh" > $bare_git_dir/hooks/post_receive
-    echo "git --work-tree=${PWD}/html/${1} checkout -f" >> $bare_git_dir/hooks/post_receive
-    chmod +x $bare_git_dir/hooks/post_receive
+    echo "#!/bin/sh" > $bare_git_dir/hooks/post-receive
+    echo "git --work-tree=${PWD}/html/${1} checkout -f" >> $bare_git_dir/hooks/post-receive
+    chmod +x $bare_git_dir/hooks/post-receive
 }
 
+# hff@www.hufeifei.cn:~/.repo/www.hufeifei.cn.git
 init_bare_git_repo www.hufeifei.cn
 init_bare_git_repo blog.hufeifei.cn
