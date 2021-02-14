c95862a45268b4ccb2d0d7ff3c9d517469fd715f
Holmofy Hu <hff1996723@163.com>
2021-02-14 17:22:09 +0800

    init-html.sh

--- init-html.sh
+++ init-html.sh
@@ -4,9 +4,11 @@
 # $user@$host:~/.repo/$1.git
 init_bare_git_repo() {
     bare_git_dir="${HOME}/.repo/${1}.git"
+    work_tree_dir="${PWD}/html/${1}"
+    mkdir -p $work_tree_dir
     git init --bare $bare_git_dir
     echo "#!/bin/sh" > $bare_git_dir/hooks/post-receive
-    echo "git --work-tree=${PWD}/html/${1} checkout -f" >> $bare_git_dir/hooks/post-receive
+    echo "git --work-tree=${work_tree_dir} --git-dir ${bare_git_dir} checkout -f" >> $bare_git_dir/hooks/post-receive
     chmod +x $bare_git_dir/hooks/post-receive
 }
 
