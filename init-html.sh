0585b8adb4384b7115f5040c0e1785cb1be9d8a3
holmofy <1938304905@qq.com>
2021-10-10 00:15:21 +0800

    Update init-html.sh

--- init-html.sh
+++ init-html.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-github_mirror=fastgit.org
-
 # access url:
 # $user@$host:~/.repo/$1.git
 init_bare_git_repo() {
-    bare_git_dir="${HOME}/.repo/${1}.git"
+    bare_git_dir="${GIT_HOME}/repo/${1}.git"
     work_tree_dir="${PWD}/html/${1}"
     mkdir -p $work_tree_dir
     git init --bare $bare_git_dir
@@ -14,18 +12,12 @@ init_bare_git_repo() {
     chmod +x $bare_git_dir/hooks/post-receive
 }
 
-init_blog_admin() {
-    repo_dir="${HOME}/.repo/admin"
-    git clone "$1" "$repo_dir" && cd "$repo_dir" && sh install-theme.sh
-    if ! command -v npm; then
-        command -v nvm || curl -o- https://raw.${github_mirror}/nvm-sh/nvm/v0.38.0/install.sh | bash
-        nvm install v14.17.0
-    fi
-    cd "$repo_dir" && npm install hexo-cli -g && npm install && hexo server --port "${2:-4000}" & 
-}
+useradd git -s /usr/bin/git-shell
+GIT_HOME=/home/git
 
 # hff@www.hufeifei.cn:~/.repo/www.hufeifei.cn.git
 init_bare_git_repo www.hufeifei.cn
 init_bare_git_repo blog.hufeifei.cn
 
-# init_blog_admin https://github.com/holmofy/blog.hufeifei.cn 4000
+chown -R git:git "${GIT_HOME}/repo"
+chown -R git:git "${PWD}/html"
